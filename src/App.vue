89850d92c09ccab8ae2f1b6c393820b79bec18a0
Rahul Mehta <rahul.mht2509@gmail.com>
2021-04-11 00:42:57 +0530

    Updated file name and added buefy support

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Dashboard msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Dashboard from './components/Dashboard.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Dashboard
   }
 }
 </script>
---
+++ src/components/Dashboard.vue
@@ -0,0 +1,40 @@
+<template>
+    <section>
+        <b-field message="What do you want to search?">
+            <b-input placeholder="Search..."
+                type="search"
+                icon="magnify">
+            </b-input>
+            <p class="control">
+                <b-button type="is-primary" label="Search" />
+            </p>
+        </b-field>
+    </section>
+</template>
+
+<script>
+export default {
+  name: 'Dashboard',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
