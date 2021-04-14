3cf8ff98eee7f50f5466d3d05fbdb04bf532177b
Rahul Mehta <rahul.mht2509@gmail.com>
2021-04-14 23:15:47 +0530

    Reactivity

--- src/components/InputContainer.vue
+++ src/components/InputContainer.vue
@@ -1,18 +1,24 @@
 <template>
     <section>
         <b-field message="What do you want to add?">
-            <b-input placeholder="Add the site..." rounded>
+            <b-input v-model="value" placeholder="Add the site..." rounded>
             </b-input>
             <p class="control">
                 <b-button type="is-primary" label="Add" />
             </p>
         </b-field>
+        <div>{{ value }} </div>
     </section>  
 </template>
 
 <script>
 export default {
   name: 'InputContainer',
+  data(){
+      return {
+        value: ''
+      }
+  },
   props: {
     msg: String
   }
